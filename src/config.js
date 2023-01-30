cd408dc55fafc401f84d8f1483e6950470ce6019
marcos <mp.dias023@gmail.com>
2023-01-30 21:09:26 +0000

    marcos change, new deploy

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const API_URL = "https://happyhack01.herokuapp.com/api";
-const SOCKET_URL = "https://happyhack01.herokuapp.com";
+const API_URL = "https://gold-panicky-parrot.cyclic.app/api";
+const SOCKET_URL = "https://gold-panicky-parrot.cyclic.app/";
 
 //const API_URL = "http://localhost:5005/api"
 //const SOCKET_URL = "http://localhost:5005"
