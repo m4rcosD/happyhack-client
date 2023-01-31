ec75b153e4ae2706631079d7399bd7dfa619aaae
marcos <mp.dias023@gmail.com>
2023-01-31 22:52:10 +0000

    new test

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const API_URL = "https://gold-panicky-parrot.cyclic.app/api";
-const SOCKET_URL = "https://gold-panicky-parrot.cyclic.app";
+const API_URL = "https://happyhack-server.vercel.app/api";
+const SOCKET_URL = "https://happyhack-server.vercel.app/";
 
 //const API_URL = "http://localhost:5005/api"
 //const SOCKET_URL = "http://localhost:5005"
